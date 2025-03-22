6b014c46a06d78eed75fa754218d432f1f59b387
Kreatronik <kreatronik@gmail.com>
2025-03-22 23:48:05 +0100

    added my usermod

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -204,6 +204,11 @@
 #ifdef USERMOD_STAIRCASE_WIPE
 #include "../usermods/stairway_wipe_basic/stairway-wipe-usermod-v2.h"
 #endif
+
+#ifdef USERMOD_WIFI_SWITCHOFF
+#include "../usermods/usermod_v2_WiFi_SwitchOff.h"
+#endif
+
 void registerUsermods()
 {
 /*
@@ -386,5 +391,9 @@ void registerUsermods()
 
   #ifdef USERMOD_STAIRCASE_WIPE
   usermods.add(new StairwayWipeUsermod());
+  #endif
+
+  #ifdef USERMOD_WIFI_SWITCHOFF
+  usermods.add(new WiFi_SwitchOff());
   #endif
 }
